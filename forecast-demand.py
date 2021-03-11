ea8037d4969e737fb5bd0f7a41b6b90f5beb66d8
seanreed1111 <seanreed1111@gmail.com>
2021-03-11 12:52:26 -0500

    data investigation

--- forecast-demand.py
+++ forecast-demand.py
@@ -13,8 +13,21 @@ dirname = "DATA"
 path = os.path.join(dirname, filename)
 
 df = pd.read_csv(path, parse_dates=['Date'])
-# print(df.info())
-cats = df[['Product_Code', 'Warehouse', 'Product_Category']].astype('category')
-# print(cats.info())
-df = pd.concat([cats, df[['Date', 'Order_Demand']]], axis='columns')
-print(df.info())
+# cats = df[['Product_Code', 'Warehouse', 'Product_Category']].astype('category')
+# df = pd.concat([cats, df[['Date', 'Order_Demand']]], axis='columns')
+
+
+df = (
+    pd.concat(
+        [
+        df[['Product_Code', 'Warehouse', 'Product_Category']].astype('category'),df[['Date', 'Order_Demand']]
+        ],
+        axis='columns')
+)
+#print(df.info())
+# There are 2160 different Product Codes
+# There are 4 different Warehouses
+# There are 33 different product categories
+for col in df.columns[:3]:
+    print(f"\n\n{df[col].unique()}")
+    
