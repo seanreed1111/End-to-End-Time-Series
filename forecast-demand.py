34bb30633744d07fdf5b157c274b1e959d68bb05
seanreed1111 <seanreed1111@gmail.com>
2021-03-11 13:20:32 -0500

    outputs transformed csv to pickle file. moves neuralprophet to if name == main block

--- forecast-demand.py
+++ forecast-demand.py
@@ -1,33 +1,38 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
 import pandas as pd
 import os
-from neuralprophet import NeuralProphet
+
 mpl.rcParams['figure.figsize'] = (10, 8)
 mpl.rcParams['axes.grid'] = False
 
-filename = "HistoricalProductDemand-kaggle-felixzhao-productdemandforecasting.csv"
-dirname = "DATA"
-path = os.path.join(dirname, filename)
+def clean_and_save():
+    filename = "HistoricalProductDemand-kaggle-felixzhao-productdemandforecasting.csv"
+    dirname = "DATA"
+    path = os.path.join(dirname, filename)
+    df = pd.read_csv(path, parse_dates=['Date'])
 
-df = pd.read_csv(path, parse_dates=['Date'])
-# cats = df[['Product_Code', 'Warehouse', 'Product_Category']].astype('category')
-# df = pd.concat([cats, df[['Date', 'Order_Demand']]], axis='columns')
+    df = (
+        pd.concat(
+            [
+            df[['Product_Code', 'Warehouse', 'Product_Category']].astype('category'),df[['Date', 'Order_Demand']]
+            ],
+            axis='columns'
+        )
+    )
 
+    # There are 2160 different Product Codes
+    # There are 4 different Warehouses
+    # There are 33 different product categories
+    # for col in df.columns[:3]:
+    #     print(f"\n\n{df[col].unique()}")
 
-df = (
-    pd.concat(
-        [
-        df[['Product_Code', 'Warehouse', 'Product_Category']].astype('category'),df[['Date', 'Order_Demand']]
-        ],
-        axis='columns')
-)
-#print(df.info())
-# There are 2160 different Product Codes
-# There are 4 different Warehouses
-# There are 33 different product categories
-for col in df.columns[:3]:
-    print(f"\n\n{df[col].unique()}")
-    
+    df = df.dropna().set_index('Date')
+    df.to_pickle("forecast_demand.pkl")
+
+if __name__ == '__main__':
+    from neuralprophet import NeuralProphet
+    new = pd.read_pickle("forecast_demand.pkl")
+    print(new.head())
+    dir(NeuralProphet)
